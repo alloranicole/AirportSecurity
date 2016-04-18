17db76fe266e9db97f1c148007440950f8428ca8
Alex Hawkins <alex@tests-MacBook-Pro.local>
2016-04-18 10:24:19 -0400

    clock?

---
+++ class_clock.php
@@ -0,0 +1,22 @@
+<?php include("class_lib.php"); ?>
+<?php
+
+   class clock {
+
+      var $sTime;
+      var $endTime;	
+
+      function initialize_start_time($startTime){
+         $this->sTime = $startTime;
+      }
+
+      function set_totalWaitTime($totalWaitTime){
+	 endTime-startTime=$totalWaitTime;
+      }
+      
+      function get_totalWaitTime(){
+	 return $this->totalWaitTime;
+      }
+   }
+
+>? 
