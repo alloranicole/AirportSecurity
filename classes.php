ad254141c54ec0eec5964710f06a57487a5da0e7
Joanna Fass <jfass@linus-public.highpoint.edu>
2016-04-28 22:04:40 -0400

    updates with comments

--- classes.php
+++ classes.php
@@ -1,18 +1,25 @@
+<!--Classes File-->
+
 <?php
          require("utility.php");
+
+	//Flight Class
+	//Holds the values that the user inputs about the flight
          class flight {
              private $Airport;
              private $Terminal;
              private $FlightTime;
              private $DayOfFlight;
-
+		
+	     //Constructor
              function __construct($airport, $terminal, $flightTime, $dayOfFlight){
                   $this->Airport = $airport;
                   $this->Terminal = $terminal;
                   $this->FlightTime = $flightTime;
                   $this->DayOfFlight = $dayOfFlight;
              }
-
+	    
+	     //Get functions
              function get_Airport(){
                   return $this->Airport;
              }
@@ -30,18 +37,36 @@
              }
 
          }
+	  
+	//Query Class
+	//Recieves information from the flight class and querys the database for the wait time
+	//that corresponds with the information from the flight class. 
+	//That wait time is then sent back to the user
           class query{
                  var $flight;
                  var $query;
                  var $result;
                  var $dbconn;
                  var $lastModified;
+
+		//Constructor
                  function __construct(flight $flightInfo){
                           $this->flight = $flightInfo;
                  }
+		
+		//connectToDB()
+		//Pre-condition: $dbconn does not hold the connection to the database
+		//Post-condition: $dbconn holds the connection to the database
                  function connectToDB(){
                    $this->dbconn = connectToDB();
                  }
+
+		//int makeQuery()
+		//Pre-condition: $dbconn is connected to the database
+		//Post-condition: $query holds the query message that was sent to the database
+		//This message includes variables from within the Instance Variable of the flight
+		//class $flight, $result holds the result from the query. The wait time from the 
+		//result is returned
                  function makeQuery(){
                   $airport = $this->flight->get_Airport();
                   $terminal = $this->flight->get_Terminal();
@@ -55,31 +80,55 @@
                    $this->lastModified = date("m.d.Y",$date);
                    return $wTime;
                  }
+
+		//modifiedLast()
+		//Post-condition: returns the date the database was last modified
                  function modifiedLast(){
                    return $this->lastModified; 
                  }
+
+		//disconnectDB()
+		//Pre-condition: $dbconn is connected to the databse
+		//Post-condition: $dbconn is no longer connected to the database
                  function disconnectDB(){
                    disconnectDB($this->dbconn);
                  }
           }
+
+   //Clock Class
+   //Holds a specific start and end time, then subtracts the two to get a total wait time,
+   //then sends it over to the wait time class, where it will be average into the database	
    class clock {
 
       private $sTime;
       private $eTime;
       
+	//set_endTime()
+	//Pre-condition: $eTime is not initialized
+	//Post-condition: $eTime is initialized with the current time
       function set_endTime(){
         $this->eTime = time();
       }
-
+	
+	//set_startTime($start)
+	//Precondition: $sTime is not initialized
+	//Postcondition: $sTime is initialized from parameters, $start
       function set_startTime($start){
       	 $this->sTime = $start;
       }
 
+	//get_startTime()
+	//Precondition: $sTime is not initialized
+	//Postcondition: $sTime is initialized to the current time and is returned
       function get_startTime(){
          $this->sTime = time();
          return $this->sTime;
       }
 
+	//get_totalWaitTime()
+	//Precondition: $totalWaitTime is not initialized
+	//Postcondition: $totalWaitTime holds $eTime - $sTime. $totalWaitTime is returned if
+	//the value is less than 120 minutes, otherwise, the function returns 0
       function get_totalWaitTime(){
          $diff = $this->eTime - $this->sTime;
          $totalWaitTime = $diff / 60;
@@ -90,6 +139,11 @@
       }
     
    }
+
+   //Wait Time Class
+   //Recieves a specified time from the clock class that the user inputs. That time is averaged
+   //into the MySQL database according to the flight info from the user. It will work 
+   //hand-in-hand with the clock class
    class waitTime{
 
       private $wTime;
@@ -97,18 +151,31 @@
       private $query;
       private $result;
       private $dbconn;
-       
+  
+	//Constructor     
       function __construct(flight $flightInfo){
          $this->flight = $flightInfo;
       }
+
+	//set_waitTime($waitTime)
+	//Precondition: $wTime is unititialized
+	//Postcondition: $wTime is initialized with the parameter, $waitTime
       function set_waitTime($waitTime){
          $this->wTime = $waitTime; 
       }
 
+	//connectToDB()
+	//Precondition: $dbconn is not connected to the database
+	//Postcondition: $dbconn is connected to the database
       function connectToDB(){
          $this->dbconn = connectToDB();
       }
 
+	//saveWaitTime()
+	//Precondition: $dbconn is connected to the database
+	//Postcondition: $query holds the query message that was sent to the databse. This message
+	//includes variables from within the Instance Variable of the Flight Class $flight, and
+	//the current wait time $wTime. $result holds the result from the query.
       function saveWaitTime(){
          $airport = $this->flight->get_Airport();
          $terminal = $this->flight->get_Terminal();
@@ -118,7 +185,13 @@
          logMsg($this->query);             
          $this->result = $this->dbconn->query($this->query);
       }
-         
+       
+	//averageWaitTime()
+	//Precondition: $dbconn is connected to the database
+	//Postcondition: $query holds the query message that was sent to the database. This message
+	//includes variables from within the Instance Variable of the flight class $flight.
+	//$result holds the result from the query. The wait time from the user is averaged into
+	//the wait time in the database  
       function averageWaitTime(){
          $airport = $this->flight->get_Airport();
          $terminal = $this->flight->get_Terminal();
@@ -138,7 +211,10 @@
           logMsg($this->query);             
           $this->result = $this->dbconn->query($this->query);
       }
-
+	
+	//disconnectDB()
+	//Precondition: $dbconn is connected to the database
+	//Postcondition: $dbconn is no longer connected to the MySQL database
       function disconnectDB(){
           disconnectDB($this->dbconn);
       }
--- output.php
+++ output.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<head>
+   <head>
        <title>Airport Security Wait Time</title>
        <meta name="viewport" content="width=device-width, initial-scale=1">
        <link rel="stylesheet" href="jquery.mobile-1.4.5.min.css" />
@@ -8,16 +8,19 @@
          src="jquery-2.2.0.min.js"></script>
         <script type="text/javascript"
          src="jquery.mobile-1.4.5.min.js"></script>
-</head>
+   </head>
 <body style="background-color: lightgrey;">
+<!--styles the header of the page-->
     <div style="background-color: rgb(0, 80, 116); color:white; padding:5px; margin:30px">
      <h1 style="text-align:center; font-family:Arial;"> Security Checkpoint Wait Time </h1>
+<!--creates and styles HOME button-->
     <div style="padding: 5px;">
-     <a class="buttons" href="index.html" style="background-color: white; color:rgb(0,80,116); border:4px solid white">HOME</a>
-   </div>
-   </div>
+     <a class="buttons" href="#input" style="background-color: white; color:rgb(0,80,116); border:4px solid white">HOME</a>
+    </div>
+   </div> <!--end header div-->
   <?php
           include("classes.php");
+	//Stores info from user input
           if($_SERVER["REQUEST_METHOD"]=="POST"){
              $airport = cleanInput($_POST["Airport"]);
              $terminal = cleanInput($_POST["Terminal"]);
@@ -40,15 +43,11 @@
              $date = $query->modifiedLast();
              $query->disconnectDB();
 
-            //Say for instance you saved the wait time in $wait, to display it:
-
-            //You can also echo html so it is displayed a certain way, like:
-  
-            
-  	?>
-            <h2 style="margin:30px; border:1px solid black; padding:5px; text-align:center;">The wait time will be around <?=$wait?> minutes.<br>		
+     ?> 
+<!-- Styles the display of the wait time -->
+           <h2 style="margin:30px; border:1px solid black; padding:5px; text-align:center;">The wait time will be around <?=$wait?> minutes.<br>
 	   <div style="font-size:50%;">
-		Last Updated: DATE   <?=$date?> 
+		Last Updated: <?=$date?> 
 		</div>
 		  </h2>
 	<?php
